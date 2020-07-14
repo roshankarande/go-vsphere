b748914932114be3a786a4e99586ecac00734058
r k <rk@gmail.com>
2020-07-14 07:03:32 -0500

    config.go changes

--- vsphere/config.go
+++ vsphere/config.go
@@ -9,42 +9,7 @@ import (
 	"net/url"
 )
 
-const (
-	envURL      = "GOVMOMI_URL"
-	envUserName = "GOVMOMI_USERNAME"
-	envPassword = "GOVMOMI_PASSWORD"
-	envInsecure = "GOVMOMI_INSECURE"
-)
-
-
-func NewClient(ctx context.Context) (*govmomi.Client, error) {
-
-	u, err := soap.ParseURL(viper.GetString("URL"))
-	if err != nil {
-		return nil, err
-	}
-
-	u.User = url.UserPassword(viper.GetString("USERNAME"),viper.GetString("PASSWORD"))
-
-	return govmomi.NewClient(ctx, u, viper.GetBool("INSECURE"))
-}
-
-func init() {
-
-	viper.SetConfigType("yaml")
-	viper.SetConfigFile("vsphere/spec.yml")
-
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println("error reading spec - ",err)
-	}
-
-	viper.SetEnvPrefix("GOVMOMI")
-	viper.AutomaticEnv()
-}
-
-func NewGovmomiClient(ctx context.Context, vSphereHost, vSphereUsername, vSpherePassword string) (*govmomi.Client, error) {
+func NewClient(ctx context.Context, vSphereHost, vSphereUsername, vSpherePassword string) (*govmomi.Client, error) {
 
 	u, err := soap.ParseURL(vSphereHost)
 	if err != nil {
